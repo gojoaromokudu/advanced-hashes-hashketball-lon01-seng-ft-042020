8d3ce2a69fc6379e8813307b3e4c8246bc224bb1
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-04-10 15:29:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -225,7 +225,7 @@ def biggest_shoe
   
   game_hash.each_value do |team|
     team[:players].each do |player|
-       binding.pry
+      # binding.pry
       if player[:shoe] > biggest_shoe_player[:shoe]
         
         biggest_shoe_player = player
